b12b5293dc1f2bf4821582e80882b219c72a16e4
TEpolito <tepolito55@gmail.com>
2017-10-23 14:41:04 -0400

    created watchSubmit function, liked to html button

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 function watchSubmit()
 {
+	$('.js-location-form').submit(function (event)
+	{
+		event.preventDefault();
 
+		const queryText = $(event.currentTarget).find(".js-location-text");
+
+		const query = queryText.val();
+
+		console.log(query);
+	});
 }
 
 $(watchSubmit());
