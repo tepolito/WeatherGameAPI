0977e32589bf743495b9aac153d6b161fcc7f349
TEpolito <tepolito55@gmail.com>
2017-10-25 20:45:04 -0400

    added ball, location working

--- index.js
+++ index.js
@@ -9,8 +9,12 @@ function getDataFromGoogleMaps(locationText)
 		{
 			if(status == 'OK')
 			{
-				console.log(results[0].geometry.location.toString(), typeof results[0].geometry.location.lat);
-				return results[0].geometry.location.toString();
+				let long = results[0].geometry.location.lng();
+				let lat = results[0].geometry.location.lat();
+
+				console.log(lat, long);
+				getDataFromDarkSky(lat, long);
+				//return results[0].geometry.location.toString();
 			}
 
 			else
@@ -21,18 +25,19 @@ function getDataFromGoogleMaps(locationText)
 	
 }
 
-function getDataFromDarkSky(location)
+function getDataFromDarkSky(lat, long)
 {
 
-	console.log(location, typeof location);
+	$('.calendar').html('');
 	for(let i =0;i<30;i++)
 	{
+		//console.log(loc, typeof loc);
 		let d =Math.floor(Date.now()/1000 + i*86400);
 		console.log(d, typeof d);
 		//i++;
-		$.ajax({
+	$.ajax({
 	    type:"GET",
-	    url: `https://api.darksky.net/forecast/6a31534b1bd30234128d6f4e569d2fa9/${location},${d}`,
+	    url: `https://api.darksky.net/forecast/6a31534b1bd30234128d6f4e569d2fa9/${lat}, ${long},${d}`,
 	    success: function(data) {
 	    	console.log(data);
 	    	addSkycon(data);
@@ -73,44 +78,45 @@ function getDataFromDarkSky(location)
 function addSkycon(data)
 {
 
-	var s1 =data.currently.icon.toUpperCase().replace(/-/g, '_') + i;
-	i++;
+	var s1 =data.currently.icon.toUpperCase().replace(/-/g, '_') + i; //create canvas id 
+	i++; //increment the id number
 	console.log(s1);
 
-	$('body').append(`<canvas id="${s1}" width="128" height="128"></canvas>`);
+	$('.calendar').append(`<canvas id="${s1}"></canvas>`);
 	
-	if(i <= 10)
+	if(i <= 10)   // take of the id number
 	{
-		var s2=s1.slice(0, -1);
+		var s2=s1.slice(0, -1); 
 	}
 	else
 	{
-		var s2=s1.slice(0,-2);
+		var s2=s1.slice(0,-2); 
 	}
 	console.log(s2, typeof s2);
 
-	skycons.add(document.getElementById(s1), Skycons[s2]);
+	skycons.add(document.getElementById(s1), Skycons[s2]); //add the skycon to the body
 	//skycons.add(s, Skycons[s]);
-	skycons.play();
+	//skycons.play(); //play the animation
 
 }
 
-function watchSubmit()
-{
-	$('.js-location-form').submit(function (event)
+//function watchSubmit()
+//{
+	$('.js-location-form').submit(function (event) //when Enter is pressed
 	{
 		event.preventDefault();
 
-		const queryText = $(event.currentTarget).find(".js-location-text");
+		const queryText = $(event.currentTarget).find(".js-location-text"); //get the value in the text box
 
 		const query = queryText.val();
 
 		console.log(query);
 
-		const loc = getDataFromGoogleMaps(query);
+		const loc = getDataFromGoogleMaps(query); //get the latitude and longitude 
 
-		getDataFromDarkSky(loc);
+		//getDataFromDarkSky(loc); //send the location data to the dark sky api
 	});
-}
+//}
 
-$(watchSubmit());
+//$(watchSubmit());
+//getDataFromGoogleMaps('seattle');
