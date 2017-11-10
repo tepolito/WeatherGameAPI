3c2fdd6d60922840034c16abaa54048a165d13ed
tepolito <tepolito55@gmail.com>
2017-11-10 16:59:33 -0500

    sd

--- index.js
+++ index.js
@@ -179,7 +179,6 @@ function watchSubmit()
 		$('.location-form').hide();
 		$('.js-calendar-info').show();
 		$('.today-container').show();
-		//$("#main").show();
 
 		//setTimeout(function(){ createToday() } , 2000);
 
@@ -189,7 +188,6 @@ function watchSubmit()
 
 function inputHandler()
 {
-	//$("#main").hide();
 	$('.js-calendar-info').hide();
 	$('.today-container').hide();
 	watchSubmit();
